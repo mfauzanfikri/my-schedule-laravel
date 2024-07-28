c26199201d58923b85a689364fd56f94cd6bbc2d
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-07-28 23:45:50 +0700

    ref: refactor layout components

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,7 @@
+<a {{ $attributes }} class="nav-link {{ $active ? '' : 'collapsed' }}">
+    @if ($icon)
+        {{ $icon }}
+    @endif
+
+    <span>{{ $slot }}</span>
+</a>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,20 +1,24 @@
 <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
-        <!-- dashboard -->
         <li class="nav-item">
-            <a class="nav-link {{ request()->is('/') ? '' : 'collapse' }}" href="/">
-                <i class="bi bi-grid"></i>
-                <span>Home</span>
-            </a>
+            <x-nav-link href="/" :active="request()->is('/')">
+                <x-slot:icon>
+                    <i class="bi bi-grid"></i>
+                </x-slot:icon>
+
+                Dashboard
+            </x-nav-link>
         </li>
 
-        <!-- logout -->
         <li class="nav-item">
-            <a class="nav-link collapsed" href="/dashboard/logout">
-                <i class="bi bi-box-arrow-left"></i>
-                <span>Logout</span>
-            </a>
+            <x-nav-link href="/logout" :active="false">
+                <x-slot:icon>
+                    <i class="bi bi-box-arrow-left"></i>
+                </x-slot:icon>
+
+                Logout
+            </x-nav-link>
         </li>
 
     </ul>
