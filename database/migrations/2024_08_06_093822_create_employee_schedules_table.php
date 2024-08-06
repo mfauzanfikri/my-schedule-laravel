4169450861c70b92ce8e68c85e173ab626f67f8d
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-08-06 16:41:45 +0700

    feat: add employee_schedules migration

---
+++ database/migrations/2024_08_06_093822_create_employee_schedules_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('employee_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained();
+            $table->date('date');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->enum('work_type', ['regular', 'shift']);
+            $table->enum('work_time', ['regular', 'morning', 'afternoon', 'night']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('employee_schedules');
+    }
+};
