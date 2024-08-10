6ecee18b49aeea67546e7efddaeadb5b440f7045
mfauzanfikri <mfauzanfikri09@gmail.com>
2024-08-10 18:24:51 +0700

    feat: add unique constraint to username field in create_users_table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_role_id')->constrained();
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
